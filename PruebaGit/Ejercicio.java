dc3bd92857a4d51a7dd8a9561afea49fd734e6ee
Erika <eperezg023@alumno.uaemex.mx>
2022-08-23 17:01:11 -0500

    Practica de git

---
+++ PruebaGit/Ejercicio.java
@@ -0,0 +1,8 @@
+package PruebaGit;
+
+public class Ejercicio {
+    public static void main(String []args){
+        System.out.println("Hola mundo");
+        System.out.println("Finalizar");
+    }
+}
